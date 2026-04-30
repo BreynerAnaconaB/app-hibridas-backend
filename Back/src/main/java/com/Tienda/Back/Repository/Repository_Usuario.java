4dcb60d92e848e3cfee9035038dcf5c05ff0840f
Julian-cryp <julianogaraytorres@gmail.com>
2026-04-29 20:51:14 -0500

    subir v3

--- Back/src/main/java/com/Tienda/Back/Repository/Repository_Usuario.java
+++ Back/src/main/java/com/Tienda/Back/Repository/Repository_Usuario.java
@@ -3,19 +3,12 @@ package com.Tienda.Back.Repository;
 import com.Tienda.Back.Modulos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
 public interface Repository_Usuario extends JpaRepository<Usuario, Long> {
-
-    // Verificaciones para el registro
     boolean existsByNombre(String nombre);
     boolean existsByEmail(String email);
-
-    // Búsqueda para el login o perfiles
     Optional<Usuario> findByNombre(String nombre);
-
-    // Agregamos este para que el login por email funcione
     Optional<Usuario> findByEmail(String email);
 }
