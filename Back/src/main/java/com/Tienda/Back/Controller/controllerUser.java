bea444ac896904a76bce4bd95b6b17182284e17e
BreynerAnaconaB <anaconabreyner29@gmail.com>
2026-05-04 03:22:42 -0500

    listo, queda desplegado

--- Back/src/main/java/com/Tienda/Back/Controller/controllerUser.java
+++ Back/src/main/java/com/Tienda/Back/Controller/controllerUser.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/user")
 public class controllerUser {
 
