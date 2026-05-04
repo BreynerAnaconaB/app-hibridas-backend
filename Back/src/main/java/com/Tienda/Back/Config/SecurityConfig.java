9221cf98bf82a5085d6a0aab271c2f48d50e6648
BreynerAnaconaB <anaconabreyner29@gmail.com>
2026-05-04 02:33:27 -0500

    garay coma mrd

--- Back/src/main/java/com/Tienda/Back/Config/SecurityConfig.java
+++ Back/src/main/java/com/Tienda/Back/Config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("*")); // 👈 usar patterns
+        config.setAllowedOriginPatterns(List.of("http://localhost:8081"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
